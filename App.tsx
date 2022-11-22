7aeafb42c08a00d29175dfd1b289817e40a7d8fe
Charles Ukasoanya <c_ukasoanya@yahoo.com>
2022-11-22 15:00:45 +0100

    setup project structure and build Home sceen for application

--- App.tsx
+++ App.tsx
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
 
-export default function App() {
+// Homescreen
+import HomeScreen from './src/screens/HomeScreen';
+
+type Props = {};
+
+const App = (props: Props) => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <HomeScreen />
     </View>
   );
-}
+};
 
+export default App;
+
+// Application styles
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
